7be76d89926040968540c7a94eaaf223ad3e9966
MysteriousLychee <70117286+MysteriousLychee@users.noreply.github.com>
2025-01-13 21:58:31 -0800

    Finish

--- main.cpp
+++ main.cpp
@@ -16,7 +16,54 @@ string strBox;
 int intRow;
 int intBox;
 
+int moves;
+
+void checkForGameOver() {
+
+    // Tie Check
+    if (moves >= 9) {
+        gameOver = true;
+    }
+
+    // Horizontal Checks
+    for (int i = 0; i < 3; i++) {
+        if (board[i][0] == board[i][1] && board[i][1] == board[i][2] &&
+            board[i][0] != ' ' && board[i][1] != ' ' && board[i][2] != ' ') {
+            cout << "Winning player is " << board[i][0] << "! Congrats!" <<endl;
+            gameOver = true;
+        }
+    }
+
+    // Vertical Checks
+    for (int i = 0; i < 3; i++) {
+        if (board[0][i] == board[1][i] && board[1][i] == board[2][i] &&
+            board[0][i] != ' ' && board[1][i] != ' ' && board[2][i] != ' ') {
+            cout << "Winning player is " << board[0][i] << "! Congrats!" << endl;
+            gameOver = true;
+        }
+    }
+
+    // Diagonal checks
+    if (board[0][0] == board[1][1] && board[1][1] == board[2][2] &&
+        board[0][0] != ' ' && board[1][1] != ' ' && board[2][2] != ' ') {
+        cout << "Winning player is " << board[1][1] << "! Congrats!" << endl;
+        gameOver = true;
+    }
+
+    if (board[0][2] == board[1][1] && board[1][1] == board[2][0] &&
+        board[0][2] != ' ' && board[1][1] != ' ' && board[2][0] != ' ') {
+        cout << "Winning player is " << board[1][1] << "! Congrats!" << endl;
+        gameOver = true;
+    }
+}
+
 void getMove() {
+
+    if (isPlayerX) {
+        cout << "It is player X's turn." << endl;
+    } else {
+        cout << "It is player O's turn." << endl;
+    }
 
     cout << "Enter the row you'd like to move in (1...3): " << endl;
     cin >> strRow;
@@ -30,8 +77,14 @@ void getMove() {
 
 void makeMove() {
 
-    board[intRow][intBox] = isPlayerX ? 'X' : 'O';
+    if (board[intRow][intBox] == ' ') {
+        board[intRow][intBox] = isPlayerX ? 'X' : 'O';
+    } else {
+        getMove();
+        makeMove();
+    }
     isPlayerX = !isPlayerX;
+    moves++;
 }
 
 void printBoard() {
@@ -47,10 +100,15 @@ void printBoard() {
 
     while (true) {
         printBoard();
-        if (!gameOver) {
-            getMove();
-            makeMove();
+        checkForGameOver();
+
+        if (gameOver) {
+            break;
         }
+
+        getMove();
+        makeMove();
+        cout << endl;
     }
 
     return 0;
